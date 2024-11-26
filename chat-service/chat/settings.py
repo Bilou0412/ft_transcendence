1132ea9980f5c098e42562587acb6f3334c7f66f
SefirOutin <1stalias.stove861@passinbox.com>
2024-11-26 18:18:39 +0100

    start changing code to interact with db

--- chat-service/chat/settings.py
+++ chat-service/chat/settings.py
@@ -87,7 +87,12 @@ WSGI_APPLICATION = 'chat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
