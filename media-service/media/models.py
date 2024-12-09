bd16b9f6efbc6c33a454efe559ac76cea71b9d0e
bilel <bilel.moudache@proton.me>
2024-12-09 14:09:56 +0100

    merge all services

--- media-service/media/models.py
+++ media-service/media/models.py
@@ -8,11 +8,11 @@ def unique_filename_username(instance, filename):
     extension = filename.split('.')[-1]
     username_slug = slugify(instance.user.username)
     unique_filename = f"{username_slug}_{uuid.uuid4()}.{extension}"
-    return os.path.join('profile_pictures', unique_filename)
+    return os.path.join('users_images', unique_filename)
 
 
 class UserProfileImage(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_pictures')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_image')
     image = models.ImageField(upload_to=unique_filename_username, null=True)
 
     def save(self, *args, **kwargs):
